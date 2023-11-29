99dd13756ef1531e923e7cd63c0b4f330fd155af
Hakase-Hamdani <mhdhamdani.formal@gmail.com>
2023-11-29 21:45:11 +0800

    commit hari rabu pt3

---
+++ admin/PasTujuanAdmin.dfm
@@ -0,0 +1,135 @@
+object frTujuan: TfrTujuan
+  Left = 740
+  Top = 203
+  Width = 928
+  Height = 480
+  Caption = 'Administrasi Tujuan'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 40
+    Top = 16
+    Width = 33
+    Height = 13
+    Caption = 'Alamat'
+  end
+  object Label2: TLabel
+    Left = 40
+    Top = 56
+    Width = 30
+    Height = 13
+    Caption = 'Orang'
+  end
+  object Label3: TLabel
+    Left = 40
+    Top = 96
+    Width = 39
+    Height = 13
+    Caption = 'Jabatan'
+  end
+  object Label4: TLabel
+    Left = 40
+    Top = 136
+    Width = 38
+    Height = 13
+    Caption = 'Institusi'
+  end
+  object Label5: TLabel
+    Left = 40
+    Top = 176
+    Width = 31
+    Height = 13
+    Caption = 'Status'
+  end
+  object edtAlamat: TEdit
+    Left = 104
+    Top = 16
+    Width = 209
+    Height = 21
+    TabOrder = 0
+  end
+  object edtOrang: TEdit
+    Left = 104
+    Top = 56
+    Width = 209
+    Height = 21
+    TabOrder = 1
+  end
+  object edtJabatan: TEdit
+    Left = 104
+    Top = 96
+    Width = 209
+    Height = 21
+    TabOrder = 2
+  end
+  object edtInstitusi: TEdit
+    Left = 104
+    Top = 136
+    Width = 209
+    Height = 21
+    TabOrder = 3
+  end
+  object edtSts: TEdit
+    Left = 104
+    Top = 176
+    Width = 209
+    Height = 21
+    TabOrder = 4
+  end
+  object DBGrid1: TDBGrid
+    Left = 336
+    Top = 16
+    Width = 545
+    Height = 177
+    DataSource = modulDB.DsTujuanAdmin
+    TabOrder = 5
+    TitleFont.Charset = DEFAULT_CHARSET
+    TitleFont.Color = clWindowText
+    TitleFont.Height = -11
+    TitleFont.Name = 'Tahoma'
+    TitleFont.Style = []
+    OnCellClick = DBGrid1CellClick
+  end
+  object btsSimpan: TButton
+    Left = 40
+    Top = 208
+    Width = 75
+    Height = 25
+    Caption = 'SIMPAN'
+    TabOrder = 6
+    OnClick = btsSimpanClick
+  end
+  object btnUbah: TButton
+    Left = 136
+    Top = 208
+    Width = 75
+    Height = 25
+    Caption = 'UBAH'
+    TabOrder = 7
+    OnClick = btnUbahClick
+  end
+  object btnHapus: TButton
+    Left = 336
+    Top = 208
+    Width = 75
+    Height = 25
+    Caption = 'HAPUS'
+    TabOrder = 8
+    OnClick = btnHapusClick
+  end
+  object edtId: TEdit
+    Left = 224
+    Top = 208
+    Width = 73
+    Height = 21
+    TabOrder = 9
+    Visible = False
+  end
+end
---
+++ admin/PasTujuanAdmin.pas
@@ -0,0 +1,100 @@
+unit PasTujuanAdmin;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Grids, DBGrids, StdCtrls;
+
+type
+  TfrTujuan = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    edtAlamat: TEdit;
+    edtOrang: TEdit;
+    edtJabatan: TEdit;
+    edtInstitusi: TEdit;
+    edtSts: TEdit;
+    DBGrid1: TDBGrid;
+    btsSimpan: TButton;
+    btnUbah: TButton;
+    btnHapus: TButton;
+    edtId: TEdit;
+    procedure btsSimpanClick(Sender: TObject);
+    procedure DBGrid1CellClick(Column: TColumn);
+    procedure btnUbahClick(Sender: TObject);
+    procedure btnHapusClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frTujuan: TfrTujuan;
+
+implementation
+
+uses
+  modulDBPas;
+
+{$R *.dfm}
+
+procedure TfrTujuan.btsSimpanClick(Sender: TObject);
+begin
+modulDB.ZqTujuanAdmin.SQL.Clear;
+modulDB.ZqTujuanAdmin.SQL.Text := '';
+modulDB.ZqTujuanAdmin.SQL.Text := 'INSERT INTO tujuan (alamat, orang, jabatan, institusi, status) VALUES (:alamat, :orang, :jabatan, :institusi, :status)';
+
+modulDB.ZqTujuanAdmin.ParamByName('alamat').Value := edtAlamat.Text;
+modulDB.ZqTujuanAdmin.ParamByName('orang').Value := edtOrang.Text;
+modulDB.ZqTujuanAdmin.ParamByName('jabatan').Value := edtJabatan.Text;
+modulDB.ZqTujuanAdmin.ParamByName('institusi').Value := edtInstitusi.Text;
+modulDB.ZqTujuanAdmin.ParamByName('status').Value := edtSts.Text;
+
+modulDB.ZqTujuanAdmin.ExecSQL;
+modulDB.DsTujuanAdmin.DataSet.Refresh;
+end;
+
+procedure TfrTujuan.DBGrid1CellClick(Column: TColumn);
+begin
+edtId.Text ;= modulDB.ZqTujuanAdmin.Fields[0].AsString;
+edtAlamat.Text := modulDB.ZqTujuanAdmin.Fields[1].AsString;
+edtOrang.Text := modulDB.ZqTujuanAdmin.Fields[2].AsString;
+edtJabatan.Text := modulDB.ZqTujuanAdmin.Fields[3].AsString;
+edtInstitusi.Text := modulDB.ZqTujuanAdmin.Fields[4].AsString;
+edtSts.Text := modulDB.ZqTujuanAdmin.Fields[5].AsString;
+end;
+
+procedure TfrTujuan.btnUbahClick(Sender: TObject);
+begin
+modulDB.ZqTujuanAdmin.SQL.Clear;
+modulDB.ZqTujuanAdmin.SQL.Text := '';
+modulDB.ZqTujuanAdmin.SQL.Text := 'UPDATE tujuan SET alamat = :tujuan, orang = :orang, jabatan = :jabatan, institusi = :institusi, status = :status WHERE id = :id';
+
+
+modulDB.ZqTujuanAdmin.ParamByName('id').Value := edtId.Text;
+modulDB.ZqTujuanAdmin.ParamByName('alamat').Value := edtAlamat.Text;
+modulDB.ZqTujuanAdmin.ParamByName('orang').Value := edtOrang.Text;
+modulDB.ZqTujuanAdmin.ParamByName('jabatan').Value := edtJabatan.Text;
+modulDB.ZqTujuanAdmin.ParamByName('institusi').Value := edtInstitusi.Text;
+modulDB.ZqTujuanAdmin.ParamByName('status').Value := edtSts.Text;
+
+modulDB.ZqTujuanAdmin.ExecSQL;
+modulDB.DsTujuanAdmin.DataSet.Refresh;
+end;
+
+procedure TfrTujuan.btnHapusClick(Sender: TObject);
+begin
+modulDB.ZqTujuanAdmin.SQL.Clear;
+modulDB.ZqTujuanAdmin.SQL.Text := '';
+modulDB.ZqTujuanAdmin.SQL.Text := 'DELETE FROM tujuan where id = :id';
+modulDB.ZqTujuanAdmin.ParamByName('id').Value := edtId.Text;
+modulDB.ZqTujuanAdmin.ExecSQL;
+modulDB.DsTujuanAdmin.DataSet.Refresh;
+end;
+
+end.
